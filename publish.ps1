7fcbe47096dbfe6bce9fe8abdd9861a4d9e6cec5
pb <pb@gauss-lvs.de>
2024-10-24 11:28:06 +0200

    Neue Version auch direkt pushen

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,3 @@
-dotnet version -s -f .\Radzen.Blazor\Radzen.Blazor.csproj build `
-  && dotnet pack .\Radzen.Blazor\Radzen.Blazor.csproj -o P:\Entwicklung\NugetPackages -c Release
+dotnet version -f .\Radzen.Blazor\Radzen.Blazor.csproj build `
+  && dotnet pack .\Radzen.Blazor\Radzen.Blazor.csproj -o P:\Entwicklung\NugetPackages -c Release `
+  && git push
