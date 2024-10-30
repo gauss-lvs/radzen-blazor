f9f7032a1daa5bcbe98c89479f592fa85cb03224
pb <pb@gauss-lvs.de>
2024-10-30 08:31:01 +0100

    Neue Version auch auf unseren Nuget-Server hochladen

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,6 @@
 dotnet version -f .\Radzen.Blazor\Radzen.Blazor.csproj build `
-  && dotnet pack .\Radzen.Blazor\Radzen.Blazor.csproj -o P:\Entwicklung\NugetPackages -c Release `
+  && dotnet pack .\Radzen.Blazor\Radzen.Blazor.csproj -o tmp_pack -c Release `
+  && Copy-Item "tmp_pack/*.nupkg" $env:GAUSS_NUGET_FOLDER `
+  && dotnet nuget push "tmp_pack/*.nupkg" --source gauss --api-key $env:GAUSS_NUGET_API_KEY --skip-duplicate `
+  && Remove-Item tmp_pack -Recurse `
   && git push
