da9316771143a51662a9aebd0a12137adb6b3751
pb <pb@gauss-lvs.de>
2025-10-22 14:49:14 +0200

    Typo

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,6 @@
 dotnet version -f .\Radzen.Blazor\Radzen.Blazor.csproj build `
   && dotnet pack .\Radzen.Blazor\Radzen.Blazor.csproj -o ".nupkgs" -c Release `
   && Copy-Item ".nupkgs\*.nupkg" $env:GAUSS_NUGET_FOLDER `
-  && dotnet nuget push ".nupkgs\*.nupkg" --source gauss --api-key $env:GAUSS_NUGET_API_KEY --skip-duplicate --no-symbols`
+  && dotnet nuget push ".nupkgs\*.nupkg" --source gauss --api-key $env:GAUSS_NUGET_API_KEY --skip-duplicate --no-symbols `
   && Remove-Item ".nupkgs" -Recurse `
   && git push
