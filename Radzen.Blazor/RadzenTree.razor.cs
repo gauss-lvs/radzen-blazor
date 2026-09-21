f3a420aca732c86bee929fdccd0e1866eaaa9b16
pb <pb@gauss-lvs.de>
2026-09-21 10:45:12 +0000

    region mit GAUSS-spezifischen Änderungen

--- Radzen.Blazor/RadzenTree.razor.cs
+++ Radzen.Blazor/RadzenTree.razor.cs
@@ -810,6 +810,8 @@ namespace Radzen.Blazor
             base.OnInitialized();
         }
 
+        #region GAUSS-spezifische Änderungen
+
         /// <summary>
         /// A callback that will be invoked when the user selects an item, before the selection is made.
         /// </summary>
@@ -834,5 +836,37 @@ namespace Radzen.Blazor
 
             return args.Cancelled;
         }
+
+        /// <summary>
+        /// Forces the item with value <paramref name="value"/> to be
+        /// re-evaluated such that items lazily created via <see cref="Expand"/>
+        /// are realised if the underlying data model has been changed from
+        /// somewhere else.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public async Task Reload(object? value = null)
+        {
+            // mde
+            // Diese Methode habe ich ergänzt, da das originale Reload() zwei Unzulänglichkeiten hat:
+            // 1) Es gibt keine Möglichkeit, von außen an ein RadzenTreeItem zu kommen.
+            // 2) Der Aufruf mit item == null funktioniert nicht mit Lazy Loading der Children, da sich die Liste 'items' dann während
+            //    der Aufzählung ändert, was eine Exception verursacht.
+
+            var item = items.FirstOrDefault(i => i.Value == value);
+            if (item != null)
+            {
+                await this.ExpandItem(item);
+            }
+            else
+            {
+                foreach (var i in this.items.ToList().Where(ri => ri.Expanded == false))
+                {
+                    await this.ExpandItem(i);
+                }
+            }
+        }
+
+        #endregion
     }
 }
